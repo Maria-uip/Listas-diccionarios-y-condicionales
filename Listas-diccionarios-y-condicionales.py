999d19bd006f3f396e7242cbf5bff7332acf5c60
Maria-uip <maria.cedenor@uip.edu.pa>
2026-03-08 17:15:31 -0500

    Estructura inicial: Lista de nombres y Diccionario de edades

---
+++ Listas-diccionarios-y-condicionales.py
@@ -0,0 +1,10 @@
+ # Lista de nombres
+nombres = ["Gian", "Adriana", "David", "Jerry", "Said", "Maria"]# Diccionario de edades# Estructura: "Nombre": Edad (entero)
+registro_edades = {
+    "Gian": 30,
+    "Adriana": 26,
+    "David": 25,
+    "Jerry": 22,
+    "Said": 35,
+    "Maria": 27
+} 
